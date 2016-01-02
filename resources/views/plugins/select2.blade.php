1188dc66622fb1768fef284f72d942c16619a66f
budi7 <budi-purnomo@outlook.com>
2016-01-02 12:57:46 +0700

    fixed register category

--- resources/views/plugins/select2.blade.php
+++ resources/views/plugins/select2.blade.php
@@ -5,7 +5,7 @@
 	$('.select2').select2();
 
 	$('.select-tag').select2({
-		placeholder: 'Masukkan nama kategori',
+		placeholder: 'Masukkan nama tag',
 		minimumInputLength: 3,
 		tags: false,
 		ajax: {
@@ -41,4 +41,42 @@
 			}
 		}
 	});
+
+	$('.select-category').select2({
+		placeholder: 'Masukkan nama kategori',
+		minimumInputLength: 3,
+		tags: false,
+		ajax: {
+			url: "{{ route('admin.ajax.category.findName') }}",
+			dataType: 'json',
+			data: function (term, path) {
+				return {
+					name: term,
+					path : '{{ isset($data['path']) ? $data['path'] : '' }}'
+				};
+			},
+		   results: function (data) {
+				return {
+					results: $.map(data, function (item) {
+						return {
+							text: item.name +' ',
+							id: item.id +' ',
+							path: item.path
+						}
+					})
+				};
+			},
+			query: function (query){
+				var data = {results: []};
+				 
+				$.each(preload_data, function(){
+					if(query.term.length == 0 || this.text.toUpperCase().indexOf(query.term.toUpperCase()) >= 0 ){
+						data.results.push({id: this.id, text: this.text });
+					}
+				});
+	
+				query.callback(data);
+			}
+		}
+	});	
 </script>
