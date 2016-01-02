df2e0b0a496ec9dc45a1af1316821d2dfecebf49
budi7 <budi-purnomo@outlook.com>
2016-01-02 12:51:17 +0700

    fixed select2 plugins

---
+++ resources/views/plugins/select2.blade.php
@@ -0,0 +1,44 @@
+{!! HTML::style('plugins/select2/select2.css') !!}
+{!! HTML::script('plugins/select2/select2.js') !!}
+
+<script>
+	$('.select2').select2();
+
+	$('.select-tag').select2({
+		placeholder: 'Masukkan nama kategori',
+		minimumInputLength: 3,
+		tags: false,
+		ajax: {
+			url: "{{ route('admin.ajax.tag.findName') }}",
+			dataType: 'json',
+			data: function (term, path) {
+				return {
+					name: term,
+					path : '{{ isset($data['path']) ? $data['path'] : '' }}'
+				};
+			},
+		   results: function (data) {
+				return {
+					results: $.map(data, function (item) {
+						return {
+							text: item.name +' ',
+							id: item.id +' ',
+							path: item.path
+						}
+					})
+				};
+			},
+			query: function (query){
+				var data = {results: []};
+				 
+				$.each(preload_data, function(){
+					if(query.term.length == 0 || this.text.toUpperCase().indexOf(query.term.toUpperCase()) >= 0 ){
+						data.results.push({id: this.id, text: this.text });
+					}
+				});
+	
+				query.callback(data);
+			}
+		}
+	});
+</script>
