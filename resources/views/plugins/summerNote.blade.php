25b5815490cab061dde6532b8b353228679a5a6e
budi7 <budi-purnomo@outlook.com>
2016-01-04 17:03:31 +0700

    fixed add summernote plugin

---
+++ resources/views/plugins/summerNote.blade.php
@@ -0,0 +1,9 @@
+{!! HTML::style('plugins/summernote/summernote.css') !!}
+{!! HTML::style('plugins/summernote/summernote-bs3.css') !!}
+{!! HTML::script('plugins/summernote/summernote.min.js') !!}
+
+<script>
+	$(document).ready(function(){
+      $('.summernote').summernote();
+	});
+</script>
