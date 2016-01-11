50a7c05cc007b72ff14ca4087c0465f9c6a87904
budi7 <budi-purnomo@outlook.com>
2016-01-11 11:25:02 +0700

    fixed redisign alertbox widget

--- resources/views/pageElements/alertbox.blade.php
+++ resources/views/pageElements/alertbox.blade.php
@@ -1,7 +1,7 @@
 @if(Session::has('msg') || $errors->any())
 	<div class="row">
 	    <div class="col-lg-12">
-	        <div class="alert alert-{{Session::get('msg-type')}} alert-dismissable">
+	        <div class="alert alert-{{Session::get('msg-type')}} alert-dismissable m-b-none" style="margin-top: 7px;">
 	            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                 @if(Session::has('msg'))
 	                {!!Session::get('msg')!!}
