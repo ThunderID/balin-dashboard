062f90fa82f14cf081e75c92162bf862a95a6075
budi7 <budi-purnomo@outlook.com>
2016-01-13 15:43:45 +0700

    fixed apicategory connector

---
+++ app/API/Connectors/APICategory.php
@@ -0,0 +1,42 @@
+<?php 
+namespace App\API\connectors;
+
+use Exception, Session;
+
+class APICategory extends APIData
+{
+	function __construct() 
+	{
+		parent::__construct();
+		$this->apiData 					= array_merge($this->apiData, ['type' => 'category']);
+	}
+
+	public function getIndex($filter = null)
+	{
+		$this->apiUrl 					= '/clusters/category';
+
+		if(!is_null($filter))
+		{
+			$this->apiData 				= array_merge($this->apiData, ["search" => $filter]);
+		}
+
+		return $this->get();
+	}
+
+	public function postData($data)
+	{
+		$this->apiUrl 				= '/cluster/category/store';
+		$this->apiData 				= array_merge($this->apiData,  ["category" => $data]);
+
+		// dd($this->apiData);
+
+		return $this->post();
+	}		
+
+	public function getShow($id)
+	{
+		$this->apiUrl 				= '/cluster/category/' . $id;
+
+		return $this->get();
+	}		
+}
