a1ea9e06d96fa95259beda1cb4ea6ea378c08154
budi7 <budi-purnomo@outlook.com>
2016-01-18 12:08:46 +0700

    fixed widget indexNavigation diabled mode

--- resources/views/pageElements/indexNavigation.blade.php
+++ resources/views/pageElements/indexNavigation.blade.php
@@ -12,45 +12,30 @@
 		$searchLabel = 'Cari Data';
 	}
 
-	if($disabled == false)
-	{
-		if(!isset($newDataRoute)){
-			array_push($errors, "Link untuk data baru tidak ada ( var : newDataRoute )");
-		}
-
-		if(!isset($filterDataRoute)){
-			array_push($errors, "Link untuk cari data tidak ada ( var : filterDataRoute )");
-		}	
+	if(!isset($newDataRoute)){
+		array_push($errors, "Link untuk data baru tidak ada ( var : newDataRoute )");
 	}
+
+	if(!isset($filterDataRoute)){
+		array_push($errors, "Link untuk cari data tidak ada ( var : filterDataRoute )");
+	}	
 ?>
 
 @if(count($errors) == 0)
 <div class="row">		
 	<div class="col-md-8 col-sm-4 hidden-xs">
-		@if($disabled ==  false)
+		@if($disabled == false)
 			<a class="btn btn-default" href="{{ $newDataRoute }}"> {{$newDataLabel}} </a>
 		@else
 			<a class="btn btn-default disabled" href="#"> {{$newDataLabel}} </a>
 		@endif
 	</div>
     <div class="col-md-4 col-sm-8 col-xs-12">
-		@if($disabled ==  false)
-			<form method="GET" action="{{ $filterDataRoute }}" accept-charset="UTF-8">
-		@endif
-		<div class="row">
-			<div class="col-md-2 col-sm-3 hidden-xs">
-			</div>
-			<div class="col-md-7 col-sm-6 col-xs-8" style="padding-right:2px;">
-				@if($disabled ==  true)
-					{!! Form::input('text', 'q', Null ,
-							[
-								'class'         => 'form-control',
-								'placeholder'   => $searchLabel,
-								'required'      => 'required',
-								'disabled'		=> 'disabled'
-							]
-					) !!} 
-				@else
+		<form method="GET" action="{{ $filterDataRoute }}" accept-charset="UTF-8">
+			<div class="row">
+				<div class="col-md-2 col-sm-3 hidden-xs">
+				</div>
+				<div class="col-md-7 col-sm-6 col-xs-8" style="padding-right:2px;">
 					{!! Form::input('text', 'q', Null ,
 							[
 								'class'         => 'form-control',
@@ -58,19 +43,12 @@
 								'required'      => 'required',
 							]
 					) !!} 
-				@endif                                
-			</div>
-			<div class="col-md-3 col-sm-3 col-xs-4" style="padding-left:2px;">
-				@if($disabled ==  true)
-					<a type="submit" class="btn btn-default pull-right btn-block disabled">Cari</a>
-				@else
+				</div>
+				<div class="col-md-3 col-sm-3 col-xs-4" style="padding-left:2px;">
 					<button type="submit" class="btn btn-default pull-right btn-block">Cari</button>
-				@endif
+				</div>
 			</div>
-		</div>
-		@if($disabled ==  false)
-			</form>
-		@endif
+		</form>
 	</div>
 </div>	
 @else
