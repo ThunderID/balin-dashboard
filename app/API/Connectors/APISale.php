6aa34df08610a1dabd0f7bc03ffaba716534daa6
chelsymooy <chelsymooy1108@gmail.com>
2016-01-26 09:26:11 +0700

    Working #8

---
+++ app/API/Connectors/APISale.php
@@ -0,0 +1,39 @@
+<?php 
+namespace App\API\Connectors;
+
+use Exception, Session;
+
+class APISale extends APIData
+{
+	function __construct() 
+	{
+		parent::__construct();
+	}
+
+	public function getIndex($parameter = null)
+	{
+		$this->apiUrl 					= '/sales';
+		
+		if(!is_null($parameter))
+		{
+			$this->apiData 				= array_merge($this->apiData, $parameter);
+		}
+
+		return $this->get();
+	}
+
+	public function postData($data)
+	{
+		$this->apiUrl 					= '/sale/update/status';
+		$this->apiData 					= array_merge($this->apiData, ["sale" => $data]);
+
+		return $this->post();
+	}	
+
+	public function getShow($id)
+	{
+		$this->apiUrl 					= '/sale/' . $id;
+
+		return $this->get();
+	}
+}
--- resources/views/pages/toko/pembelian/index.blade.php
+++ resources/views/pages/toko/pembelian/index.blade.php
@@ -10,8 +10,88 @@
 	</div>
 <!-- end of head -->
 
+@extends('page_templates.layout')
+@section('content')
+<div class="container-fluid">
+<!-- head -->
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 m-b-md border-bottom">
+			@include('pageElements.pagetitle')
+			@include('pageElements.breadcrumb')
+		</div>
+	</div>
+<!-- end of head -->
+
 <!-- content -->
-	pembelian
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="table-responsive">
+				<table class="table table-bordered table-hover table-striped">
+					<thead>
+						<tr>
+							<th class="text-center">
+								No.
+							</th>
+							<th class="col-md-2 text-left">
+								#
+							</th>
+							<th class="col-md-4">
+								Nama Customer
+							</th>
+							<th class="col-md-2 text-center">
+								Tagihan
+							</th>
+							<th class="col-md-2 text-center">
+								Status
+							</th>
+							<th class="text-center">
+								Kontrol
+							</th>							
+						</tr>
+					</thead>
+					<tbody>
+						@if(count($data['sale']['data']['data']) == 0)
+							<tr>
+								<td colspan="6" class="text-center">
+									Tidak ada data
+								</td>
+							</tr>
+						@else                                                                 
+							@foreach($data['sale']['data']['data'] as $key => $dt)
+								<tr>
+									<td class="text-center">
+										{{ ($paging->perPage() * ($paging->currentPage() - 1)) + $key + 1}}
+									</td>
+									<td>
+										{{ $dt['ref_number'] }}
+									</td>
+									<td>
+										{{ $dt['user']['name'] }}
+									</td>
+									<td class="text-right">
+										@money_indo($dt['bills'])
+									</td>
+									<td class="text-center">
+										{{$dt['status']}}
+									</td>
+									<td class="text-center">
+										<a href="{{ route('admin.sell.show', $dt['id']) }}"> Detail</a>,
+										<a href="{{ route('admin.sell.edit', $dt['id']) }}"> Edit</a> 
+									</td>    
+								</tr>       
+							@endforeach 
+						@endif
+						
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 hollow-pagination" style="text-align:right;">
+			{!! $paging->appends(Input::all())->render() !!}
+		</div>	
+	</div>	
 <!-- end of content -->
 
 </div>
