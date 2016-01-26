f976dbae10e67289db8043fb4e521fe1886ab8d5
chelsymooy <chelsymooy1108@gmail.com>
2016-01-26 10:54:38 +0700

    adding search to discount

--- resources/views/pages/promosi/diskon/index.blade.php
+++ resources/views/pages/promosi/diskon/index.blade.php
@@ -8,6 +8,17 @@
 			@include('pageElements.breadcrumb')
 		</div>
 	</div>
+
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 m-b-md">
+			@include('pageElements.indexNavigation', [
+				'newDataRoute' 		=> route('admin.product.create'),
+				'filterDataRoute' 	=> route('admin.discount.index')
+			])
+			@include('pageElements.searchResult', ['closeSearchLink' => route('admin.discount.index') ])
+		</div>
+	</div>
+	</br> 	
 <!-- end of head -->
 
 	<div class="row">
