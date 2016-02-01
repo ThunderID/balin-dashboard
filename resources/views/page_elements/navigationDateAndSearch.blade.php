dee4c311844db97643ce031108cd601e06d2a506
budi7 <budi-purnomo@outlook.com>
2016-02-01 15:50:45 +0700

    fixed add widget navigation date range responsive

--- resources/views/page_elements/navigationDateAndSearch.blade.php
+++ resources/views/page_elements/navigationDateAndSearch.blade.php
@@ -1,11 +1,11 @@
 <div class="row">
-	<div class="col-md-4">
-		<div class="row">
-			<form action='javascript:void(0)' onSubmit="ajaxMonthYearRange(this);">
-				<div class="col-md-3">
+	<div class="col-md-4 col-sm-5 col-xs-12">
+		<form action='javascript:void(0)' onSubmit="ajaxMonthYearRange(this);">
+			<div class="row">
+				<div class="col-md-3 col-sm-12 col-xs-12">
 					<h3 style="margin-top:7px;">Periode</h3>
 				</div>
-				<div class="col-md-7">
+				<div class="col-md-6 col-sm-8 col-xs-8" style="padding-right:2px;">
 					@if(Input::get('periode'))
 					<?php 
 						$range = Input::get('periode');
@@ -21,20 +21,23 @@
 						'id'			=> 'monthyear'
 					]) !!}
 				</div>
-				<div class="col-md-2">
+				<div class="col-md-3 col-sm-4 col-xs-4" style="padding-left:2px;">
 					<button type="submit" class="btn btn-default pull-right btn-block">Go</button>
 				</div>
-			</form>
-		</div>
+			</div>
+		</form>
 	</div>
-	<div class="col-md-2">
+	<div class="col-md-2 col-sm-1 col-xs-12 m-b-md">
 	</div>
-	<div class="col-md-6">
+	<div class="col-md-6 col-sm-6 col-xs-12">
 		<form action='javascript:void(0)' onSubmit="ajaxFilterSearch(this);">
 			<div class="row">
-				<div class="col-md-3">
+				<div class="hidden-lg hidden-md col-sm-12 hidden-xs">
+					<h3 style="margin-top:7px;">&nbsp;</h3>
 				</div>
-				<div class="col-md-7 col-sm-7 col-xs-4" style="padding-right:2px;">
+				<div class="col-md-3 col-sm-3 hidden-xs">
+				</div>
+				<div class="col-md-7 col-sm-7 col-xs-8" style="padding-right:2px;">
 					{!! Form::input('text', 'q', Null ,
 							[
 								'id'			=> 'data-search',
@@ -44,7 +47,7 @@
 							]
 					) !!} 
 				</div>
-				<div class="col-md-2 col-sm-2 col-xs-3" style="padding-left:2px;">
+				<div class="col-md-2 col-sm-2 col-xs-4" style="padding-left:2px;">
 					<button type="submit" class="btn btn-default pull-right btn-block"><i class="fa fa-search"></i></button>
 				</div>
 			</div>
