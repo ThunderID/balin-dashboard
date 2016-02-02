f1b2c96d8ec1f26f59abd3a5c99fed569d049167
budi7 <budi-purnomo@outlook.com>
2016-02-02 15:19:42 +0700

    fixed page customer referral index

--- resources/views/pages/kostumer/referral/index.blade.php
+++ resources/views/pages/kostumer/referral/index.blade.php
@@ -14,75 +14,82 @@
 			@include('page_elements.indexNavigation', [
 				'disabled' 			=> true,
 				'newDataRoute' 		=> '/',
-				'filterDataRoute' 	=> route('customer.referral.index')
-			])
-			@include('page_elements.searchResult', [
-				'closeSearchLink' 	=> route('customer.referral.index') 
+				'filterDataRoute' 	=> route('customer.referral.index'),
+				'filters'			=> []
 			])
 		</div>
 	</div>
-	</br>
+
+	<div id="contentData">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">			
+				@include('page_elements.searchResult', [
+					'closeSearchLink' 	=> route('customer.referral.index') 
+				])
+			</div>
+		</div>
+		</br>
 <!-- end of head -->
 
 <!-- content -->
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="table-responsive">
-				<table class="table table-bordered table-hover table-striped">
-					<thead>
-						<tr>
-							<th class="col-md-1 text-center">No.</th>
-							<th class="col-md-2 text-center">Nama</th>
-							<th class="col-md-2 text-center">Email</th>
-							<th class="col-md-2 text-center">Total Referral</th>
-							<th class="col-md-3 text-center">Referral</th>
-							<th class="text-center col-md-2">Kontrol</th>
-						</tr>
-					</thead>
-					<tbody>
-						@if(count($data['customer']['data']['data']) == 0)
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="table-responsive">
+					<table class="table table-bordered table-hover table-striped">
+						<thead>
 							<tr>
-								<td colspan="11" class="text-center">
-									Tidak ada data
-								</td>
+								<th class="col-md-1 text-center">No.</th>
+								<th class="col-md-2 text-center">Nama</th>
+								<th class="col-md-2 text-center">Email</th>
+								<th class="col-md-2 text-center">Total Referral</th>
+								<th class="col-md-3 text-center">Referral</th>
+								<th class="text-center col-md-2">Kontrol</th>
 							</tr>
-						@else
-							@foreach($data['customer']['data']['data'] as $key => $dt)
+						</thead>
+						<tbody>
+							@if(count($data['customer']['data']['data']) == 0)
 								<tr>
-									<td class="text-center">
-										{{ ($paging->perPage() * ($paging->currentPage() - 1)) + $key + 1}}
-									</td>
-									<td class="text-left">
-										{{ $dt['name'] }}
-									</td>
-									<td class="text-left">
-										{{ $dt['email'] }}
-									</td>
-									<td class="text-center">
-										{{ $dt['total_reference'] }}
-									</td>
-									<td class="text-center">
-										@foreach($dt['myreferrals'] as $key => $value)
-											<p>{{$value['user']['name']}}</p>
-										@endforeach
-									</td>
-									<td class="text-center">
-										<a href="{{ route('customer.customer.show', $dt['id']) }}"> Detail</a>
+									<td colspan="11" class="text-center">
+										Tidak ada data
 									</td>
 								</tr>
-							@endforeach 
-						@endif
-				
-					</tbody>
-				</table>
+							@else
+								@foreach($data['customer']['data']['data'] as $key => $dt)
+									<tr>
+										<td class="text-center">
+											{{ ($paging->perPage() * ($paging->currentPage() - 1)) + $key + 1}}
+										</td>
+										<td class="text-left">
+											{{ $dt['name'] }}
+										</td>
+										<td class="text-left">
+											{{ $dt['email'] }}
+										</td>
+										<td class="text-center">
+											{{ $dt['total_reference'] }}
+										</td>
+										<td class="text-center">
+											@foreach($dt['myreferrals'] as $key => $value)
+												<p>{{$value['user']['name']}}</p>
+											@endforeach
+										</td>
+										<td class="text-center">
+											<a href="{{ route('customer.customer.show', $dt['id']) }}"> Detail</a>
+										</td>
+									</tr>
+								@endforeach 
+							@endif
+					
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-12 hollow-pagination" style="text-align:right;">
-			{!! $paging->appends(Input::all())->render() !!}
+		<div class="row">
+			@include('page_elements.ajaxPaging')
 		</div>	
-	</div>	
 <!-- end of content -->
+
+	</div>
 </div>
 @stop
--- resources/views/pages/laporan/product/index.blade.php
+++ resources/views/pages/laporan/product/index.blade.php
@@ -24,6 +24,11 @@
 				'filterDataRoute' 	=> route('report.product.sold'),
 				'searchLabel' 		=> 'dd-mm-yyyy to dd-mm-yyyy'
 			])
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">			
 			@include('page_elements.searchResult', ['closeSearchLink' => route('report.product.sold') ])
 		</div>
 	</div>
