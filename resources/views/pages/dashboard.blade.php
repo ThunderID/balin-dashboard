e079330941b196db7a9d6d2919a962ce4ba44542
budi7 <budi-purnomo@outlook.com>
2016-02-11 12:16:49 +0700

    fixed gulp-ing table

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,109 +1,83 @@
 @extends('page_templates.layout')
 @section('content')
 <div class="container-fluid">
-<!-- head -->
+
+{{-- Head --}}
 	<div class="row">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 m-b-md border-bottom">
 			@include('page_elements.pagetitle')
 			@include('page_elements.breadcrumb')
 		</div>
 	</div>
-<!-- end of head -->
+{{-- End of head --}}
 
-<!-- content -->
-	<!-- pekerjaan hari ini -->
+{{-- Content --}}
+	{{-- Pekerjaan hari ini --}}
 	<div class="row">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 			<h3>Pekerjaan Hari Ini</h3>
 		</div>
 	</div>
+
 	<div class="row clearfix">
 		&nbsp;
 	</div>
+	
 	<div class="row">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 m-b-xs">
 			<div role="tabpanel">
-		<!-- Nav tabs -->
+		{{-- Nav tabs --}}
 				<ul class="nav nav-tabs" role="tablist">
 					<li role="presentation" class="active">
-						<a href="#barang" aria-controls="barang" role="tab" data-toggle="tab">Barang ({{$data['warehouse']['count']}})</a>
+						<a href="#toko" aria-controls="toko" role="tab" data-toggle="tab">
+							Toko ({{$data['shipped']['count'] + $data['packed']['count'] + $data['paid']['count'] + $data['wait']['count']}})
+						</a>
 					</li>
 					<li role="presentation">
-						<a href="#toko" aria-controls="toko" role="tab" data-toggle="tab">Toko ({{$data['shipped']['count'] + $data['packed']['count'] + $data['paid']['count'] + $data['wait']['count']}})</a>
+						<a href="#barang" aria-controls="barang" role="tab" data-toggle="tab">
+							Barang ({{$data['warehouse']['count']}})
+						</a>
 					</li>
 					 <li role="presentation">
-						<a href="#laporan" aria-controls="tab" role="tab" data-toggle="tab">Laporan (0)</a>
+						<a href="#laporan" aria-controls="tab" role="tab" data-toggle="tab">
+							Laporan (0)
+						</a>
 					</li>
 					<li role="presentation">
-						<a href="#pengaturan" aria-controls="tab" role="tab" data-toggle="tab">Pengaturan (0)</a>
+						<a href="#pengaturan" aria-controls="tab" role="tab" data-toggle="tab">
+							Pengaturan (0)
+						</a>
 					</li>
 					<li role="presentation">
-						<a href="#customer" aria-controls="tab" role="tab" data-toggle="tab">Customer (0)</a>
+						<a href="#customer" aria-controls="tab" role="tab" data-toggle="tab">
+							Customer (0)
+						</a>
 					</li>
 					<li role="presentation">
-						<a href="#promosi" aria-controls="tab" role="tab" data-toggle="tab">Promosi (0)</a>
+						<a href="#promosi" aria-controls="tab" role="tab" data-toggle="tab">
+							Promosi (0)
+						</a>
 					</li>                    
 				</ul>
-		<!-- end of nav tabs -->
-			
-		<!-- Tab panes -->
+		{{-- End of nav tabs --}}
+
+		{{-- Tab panes --}}
 				<div class="tab-content">
-			<!-- tab barang -->
-					<div role="tabpanel" class="tab-pane active" id="barang">
-						<div class="panel-group" id="accordionBarang" style="margin-top:5px;">
-							
-							<div class="panel panel-default dahboard-list">
-								<a data-toggle="collapse" data-parent="#accordionBarang" href="#collapseb1">
-									<div class="panel-heading">
-										<h4 class="panel-title">
-											Re-Stok Produk ({{$data['warehouse']['count']}})
-										</h4>
-									</div>
-								</a>
-								<div id="collapseb1" class="panel-collapse collapse">
-		                            <table class="table table-bordered table-hover table-striped">
-		                                <tbody>
-											@forelse($data['warehouse']['data'] as $key => $value)
-												<tr>
-			                                        <td class="col-xs-1" style="padding-left: 25px !important;">
-			                                            {{ $key + 1 }}
-			                                        </td>
-			                                        <td>
-			                                            {{$value['product']['name']}}
-			                                        </td>
-			                                        <td class="col-xs-1"><a href="{{route('shop.buy.create', ['pid' => $value['product']['id']])}}">Kerjakan</a></td>
-												</tr>
-											@empty
-												<tr>
-			                                        <td class="col-xs-1" style="padding-left: 25px !important;">
-			                                            Tidak ada pekerjaan untuk sekarang.
-			                                        </td>
-			                                    </tr>
-											@endforelse
-		                                </tbody>
-		                            </table> 
-								</div>
-							</div>
+			{{-- Toko --}}
+					<div role="tabpanel" class="tab-pane active" id="toko">
+						<div class="panel-group" style="margin-top:5px;">
 
-						</div>
-					</div>
-			<!-- end of tab barang -->
-
-			<!-- tab toko -->
-					<div role="tabpanel" class="tab-pane" id="toko">
-						<div class="panel-group" id="accordionToko" style="margin-top:5px;">
-							
+				{{-- Validasi bayar --}}
 							<div class="panel panel-default dahboard-list">
-								
-								<a data-toggle="collapse" data-parent="#accordionToko" href="#collapset1">
+								<a data-toggle="collapse" data-target="#collapse_toko_1">
 									<div class="panel-heading">
 										<h4 class="panel-title">
 											Validasi Bayar ({{$data['wait']['count']}})
 										</h4>
 									</div>
 								</a>
-								<div id="collapset1" class="panel-collapse collapse">
-		                            <table class="table table-bordered table-hover table-striped">
+								<div id="collapse_toko_1" class="collapse {{$data['wait']['count'] > 0?'in':'yes'}}">
+		                            <table class="table table-hover">
 		                                <tbody>
 											@forelse($data['wait']['data'] as $key => $value)
 												<tr>
@@ -117,7 +91,7 @@
 			                                    </tr>
 											@empty
 												<tr>
-			                                        <td class="col-xs-1" style="padding-left: 25px !important;">
+			                                        <td class="col-xs-1 text-center" style="padding-left: 25px !important;">
 			                                            Tidak ada pekerjaan untuk sekarang.
 			                                        </td>
 			                                    </tr>
@@ -125,20 +99,21 @@
 		                                </tbody>
 		                            </table> 
 								</div>
-
 							</div>
+				{{-- End of validasi bayar --}}
 
+
+				{{-- Packing --}}
 							<div class="panel panel-default dahboard-list">
-								
-								<a data-toggle="collapse" data-parent="#accordionToko" href="#collapset2">
+								<a data-toggle="collapse" data-target="#collapse_toko_2">
 									<div class="panel-heading">
 										<h4 class="panel-title">
 											Packing ({{$data['paid']['count']}})
 										</h4>
 									</div>
 								</a>
-								<div id="collapset2" class="panel-collapse collapse">
-		                            <table class="table table-bordered table-hover table-striped">
+								<div id="collapse_toko_2" class="collapse {{ ($data['paid']['count'] > 0)?'in':'' }}">
+		                            <table class="table table-hover">
 		                                <tbody>
 											@forelse($data['paid']['data'] as $key => $value)
 												<tr>
@@ -152,7 +127,7 @@
 			                                    </tr>
 											@empty
 												<tr>
-			                                        <td class="col-xs-1" style="padding-left: 25px !important;">
+			                                        <td class="col-xs-1 text-center" style="padding-left: 25px !important;">
 			                                            Tidak ada pekerjaan untuk sekarang.
 			                                        </td>
 			                                    </tr>
@@ -160,20 +135,20 @@
 		                                </tbody>
 		                            </table> 
 								</div>
-								
-							</div>	
+							</div>
+				{{-- End of packing --}}
 
+				{{-- Kirim --}}
 							<div class="panel panel-default dahboard-list">
-								
-								<a data-toggle="collapse" data-parent="#accordionToko" href="#collapset3">
+								<a data-toggle="collapse" href="#collapse_toko_3">
 									<div class="panel-heading">
 										<h4 class="panel-title">
 											Kirim Barang ({{$data['packed']['count']}})
 										</h4>
 									</div>
 								</a>
-								<div id="collapset3" class="panel-collapse collapse">
-		                            <table class="table table-bordered table-hover table-striped">
+								<div id="collapse_toko_3" class="collapse">
+		                            <table class="table table-hover">
 		                                <tbody>
 											@forelse($data['packed']['data'] as $key => $value)
 												<tr>
@@ -187,7 +162,7 @@
 			                                    </tr>
 											@empty
 												<tr>
-			                                        <td class="col-xs-1" style="padding-left: 25px !important;">
+			                                        <td class="col-xs-1 text-center" style="padding-left: 25px !important;">
 			                                            Tidak ada pekerjaan untuk sekarang.
 			                                        </td>
 			                                    </tr>
@@ -195,20 +170,20 @@
 		                                </tbody>
 		                            </table> 
 								</div>
-								
-							</div>														
+							</div>
+				{{-- End of kirim --}}
 
+				{{-- Barang diterima --}}
 							<div class="panel panel-default dahboard-list">
-								
-								<a data-toggle="collapse" data-parent="#accordionToko" href="#collapset4">
+								<a data-toggle="collapse"  href="#collapse_toko_4">
 									<div class="panel-heading">
 										<h4 class="panel-title">
-											Transaksi Selesai ({{$data['shipped']['count']}})
+											Validasi barang diterima ({{$data['shipped']['count']}})
 										</h4>
 									</div>
 								</a>
-								<div id="collapset4" class="panel-collapse collapse">
-		                            <table class="table table-bordered table-hover table-striped">
+								<div id="collapse_toko_4" class="panel-collapse collapse">
+		                            <table class="table table-hover">
 		                                <tbody>
 											@forelse($data['shipped']['data'] as $key => $value)
 												<tr>
@@ -222,7 +197,7 @@
 			                                    </tr>
 											@empty
 												<tr>
-			                                        <td class="col-xs-1" style="padding-left: 25px !important;">
+			                                        <td class="col-xs-1 text-center" style="padding-left: 25px !important;">
 			                                            Tidak ada pekerjaan untuk sekarang.
 			                                        </td>
 			                                    </tr>
@@ -230,87 +205,152 @@
 		                                </tbody>
 		                            </table> 
 								</div>
-								
 							</div>		
+				{{-- End of barang diterima --}}
 
 						</div>
 					</div>
-			<!-- end of tab toko -->
+			{{-- End of tab toko --}}
 
-			<!-- tab laporan -->
+			{{-- Tab barang --}}
+					<div role="tabpanel" class="tab-pane" id="barang">
+						<div class="panel-group" style="margin-top:5px;">
+
+				{{-- Restock barang --}}
+							<div class="panel panel-default dahboard-list">
+								<a data-toggle="collapse" href="#collapse_barang_1">
+									<div class="panel-heading">
+										<h4 class="panel-title">
+											Re-Stok Produk ({{$data['warehouse']['count']}})
+										</h4>
+									</div>
+								</a>
+								<div id="collapse_barang_1" class="collapse {{ ($data['warehouse']['count'] > 0)?'in':'' }}">
+		                            <table class="table table-hover">
+		                                <tbody>
+											@forelse($data['warehouse']['data'] as $key => $value)
+												<tr>
+			                                        <td class="col-xs-1" style="padding-left: 25px !important;">
+			                                            {{ $key + 1 }}
+			                                        </td>
+			                                        <td>
+			                                            {{$value['product']['name']}}
+			                                        </td>
+			                                        <td class="col-xs-1"><a href="{{route('shop.buy.create', ['pid' => $value['product']['id']])}}">Kerjakan</a></td>
+												</tr>
+											@empty
+												<tr>
+			                                        <td class="col-xs-1 text-center" style="padding-left: 25px !important;">
+			                                            Tidak ada pekerjaan untuk sekarang.
+			                                        </td>
+			                                    </tr>
+											@endforelse
+		                                </tbody>
+		                            </table> 
+								</div>
+							</div>
+				{{-- End of restock barang --}}
+
+						</div>
+					</div>
+			{{-- End of tab barang --}}
+
+			{{-- Tab laporan --}}
 					<div role="tabpanel" class="tab-pane" id="laporan">
-						<div class="panel-group" id="accordionLaporan" style="margin-top:5px;">
-                            <table class="table table-bordered table-hover table-striped">
-                                <tbody>
-									<tr>
-	                                    <td class="col-xs-1" style="padding-left: 25px !important;">
-	                                        Tidak ada pekerjaan untuk sekarang.
-	                                    </td>
-	                                </tr>
-	                            </tbody>
-	                         </table>                                	
+						<div class="panel-group" style="margin-top:5px;">
+
+				{{-- None --}}
+							<div class="panel panel-default dahboard-list">
+	                            <table class="table table-hover">
+	                                <tbody>
+										<tr>
+		                                    <td class="col-xs-1 text-center" style="padding-left: 25px !important;">
+		                                        Tidak ada pekerjaan untuk sekarang.
+		                                    </td>
+		                                </tr>
+		                            </tbody>
+		                         </table>                                	
+							</div>
+				{{-- End of none --}}
+
 						</div>
 					</div>
-			<!-- end of tab laporan -->
+			{{-- End of tab laporan --}}
 
-			<!-- tab pengaturan -->
+			{{-- Tab pengaturan --}}
 					<div role="tabpanel" class="tab-pane" id="pengaturan">
-						<div class="panel-group" id="accordionPengaturan" style="margin-top:5px;">
-                            <table class="table table-bordered table-hover table-striped">
-                                <tbody>
-									<tr>
-	                                    <td class="col-xs-1" style="padding-left: 25px !important;">
-	                                        Tidak ada pekerjaan untuk sekarang.
-	                                    </td>
-	                                </tr>
-	                            </tbody>
-	                         </table> 
+						<div class="panel-group" style="margin-top:5px;">
+
+				{{-- None --}}
+							<div class="panel panel-default dahboard-list">
+	                            <table class="table table-hover">
+	                                <tbody>
+										<tr>
+		                                    <td class="col-xs-1 text-center" style="padding-left: 25px !important;">
+		                                        Tidak ada pekerjaan untuk sekarang.
+		                                    </td>
+		                                </tr>
+		                            </tbody>
+		                         </table> 
+							</div>
+				{{-- End of none --}}
+
 						</div>
 					</div>
-			<!-- end of tab pengaturan -->
+			{{-- End of tab pengaturan --}}
 
-			<!-- tab customer -->
+			{{-- Tab customer --}}
 					<div role="tabpanel" class="tab-pane" id="customer">
-						<div class="panel-group" id="accordionCustomer" style="margin-top:5px;">
-                            <table class="table table-bordered table-hover table-striped">
-                                <tbody>
-									<tr>
-	                                    <td class="col-xs-1" style="padding-left: 25px !important;">
-	                                        Tidak ada pekerjaan untuk sekarang.
-	                                    </td>
-	                                </tr>
-	                            </tbody>
-	                         </table> 
+						<div class="panel-group" style="margin-top:5px;">
+
+				{{-- None --}}
+							<div class="panel panel-default dahboard-list">
+	                            <table class="table table-hover">
+	                                <tbody>
+										<tr>
+		                                    <td class="col-xs-1 text-center" style="padding-left: 25px !important;">
+		                                        Tidak ada pekerjaan untuk sekarang.
+		                                    </td>
+		                                </tr>
+		                            </tbody>
+		                        </table> 
+							</div>
+				{{-- End of none --}}
+
 						</div>
 					</div>
-			<!-- end of tab customer -->
-					
+			{{-- End of tab customer --}}
 
-			<!-- tab promosi -->
+			{{-- Tab promosi --}}
 					<div role="tabpanel" class="tab-pane" id="promosi">
-						<div class="panel-group" id="accordionPromosi" style="margin-top:5px;">
-                            <table class="table table-bordered table-hover table-striped">
-                                <tbody>
-									<tr>
-	                                    <td class="col-xs-1" style="padding-left: 25px !important;">
-	                                        Tidak ada pekerjaan untuk sekarang.
-	                                    </td>
-	                                </tr>
-	                            </tbody>
-	                         </table> 
+						<div class="panel-group" style="margin-top:5px;">
+
+				{{-- None --}}
+							<div class="panel panel-default dahboard-list">
+	                            <table class="table table-hover">
+	                                <tbody>
+										<tr>
+		                                    <td class="col-xs-1 text-center" style="padding-left: 25px !important;">
+		                                        Tidak ada pekerjaan untuk sekarang.
+		                                    </td>
+		                                </tr>
+		                            </tbody>
+		                         </table> 
+	                        </div>
+				{{-- End of none --}}
+
 						</div>
-					</div>					
-			<!-- end of tab promosi -->
+					</div>	
+			{{-- End of tab promosi --}}
 
 				</div>
-		<!-- end of tab panes -->
+		{{-- End of tab panes --}}
 
 			</div>
 		</div>
 	</div>
-	<!-- end of pekerjaan hari ini -->
+	{{-- End of pekerjaan hari ini --}}
+{{-- End of content --}}
 
-	
-<!-- end of content -->
 </div>
 @stop
