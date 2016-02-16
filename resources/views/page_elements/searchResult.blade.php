1b135595c845b28d1c77c6a4f73220021868bac8
budi7 <budi-purnomo@outlook.com>
2016-02-16 11:57:18 +0700

    fixed UI search result

--- resources/views/page_elements/searchResult.blade.php
+++ resources/views/page_elements/searchResult.blade.php
@@ -1,8 +1,10 @@
 @if(isset($searchResult))
 </br>
-<div class="row">
-	<div class="col-md-12">
-		Menampilkan data pencarian "{{$searchResult}}" (<a href="javascript:void(0)" onClick="ajaxClearSearch()">close</a>)
+	<div class="row">
+	    <div class="col-lg-12">
+	        <div class="alert alert-info m-b-none" style="margin-top: 7px;">
+				Menampilkan data pencarian "{{$searchResult}}" (<a href="javascript:void(0)" onClick="ajaxClearSearch()">close</a>)
+	        </div>
+	    </div>
 	</div>
-</div>
 @endif
