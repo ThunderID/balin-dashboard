ae7c93f177bb1dccd64bb600179fb246c399544b
budi7 <budi-purnomo@outlook.com>
2016-02-17 16:04:07 +0700

    fixed rekap penjualan & penggunaan voucher

--- app/Http/Controllers/Laporan/RecapController.php
+++ app/Http/Controllers/Laporan/RecapController.php
@@ -67,8 +67,6 @@ class RecapController extends AdminController
 
 
 		$this->page_attributes->filters				= 	[
-															'titles' 		=> ['pembayaran'], 
-															'pembayaran' 	=> ['Cash','Voucher']
 														];
 
 
